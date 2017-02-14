1cd38deecb42a1774201a514a4a7b6b5cbf1e10e
Tyler Blount <trblount@mines.edu>
2017-02-14 12:12:58 -0700

    Finished part 1 of the project

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,21 +1,37 @@
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class ScoreTrakker {
-	private ArrayList<Student> Students;
+	private ArrayList<Student> Students = new ArrayList<Student>();
 	
 	public ScoreTrakker() {
 	}
 	
-	private void loadDataFromFile(String filename) {
-		
+	private void loadDataFromFile(String filename) throws FileNotFoundException {
+		FileReader reader = new FileReader(filename);
+		Scanner in = new Scanner(reader);
+		while(in.hasNextLine()) {
+			String name = in.nextLine();
+			int score = Integer.parseInt(in.nextLine());
+			Student s = new Student(name, score);
+			Students.add(s);
+		}
 	}
 	
 	private void printInOrder() {
-		
+		Collections.sort(Students);
+		for (Student s : Students) {
+			System.out.println(s);
+		}
 	}
 	
 	public void processFiles() {
-		
+		try {
+			loadDataFromFile("scores.txt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		printInOrder();
 	}
 	
 	public static void main(String [] args) {

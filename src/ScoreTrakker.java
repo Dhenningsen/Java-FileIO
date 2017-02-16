1138803bfa9696334b3372d48bf2c24b361f45a8
David Henningsen <henningsen@mymail.mines.edu>
2017-02-16 15:15:30 -0700

    Finished part 2 of the assignment

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -4,34 +4,51 @@ import java.io.*;
 public class ScoreTrakker {
 	private ArrayList<Student> Students = new ArrayList<Student>();
 	
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt" }; 
+	
 	public ScoreTrakker() {
 	}
 	
 	private void loadDataFromFile(String filename) throws FileNotFoundException {
+		Students.clear();
+		
 		FileReader reader = new FileReader(filename);
 		Scanner in = new Scanner(reader);
+		String name = "";
+		String line = "";
+		
 		while(in.hasNextLine()) {
-			String name = in.nextLine();
-			int score = Integer.parseInt(in.nextLine());
-			Student s = new Student(name, score);
-			Students.add(s);
+			name = in.nextLine();
+			line = in.nextLine();
+			try{
+				int score = Integer.parseInt(line);
+				Student s = new Student(name, score);
+				Students.add(s);
+			}
+			catch(NumberFormatException e){
+				System.out.println("Incorrect format for " + name + " not a valid score : " + line + "\n");
+			}
 		}
 	}
 	
 	private void printInOrder() {
 		Collections.sort(Students);
+		System.out.println("Student score list");
 		for (Student s : Students) {
 			System.out.println(s);
 		}
 	}
 	
 	public void processFiles() {
-		try {
-			loadDataFromFile("scores.txt");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		for(String s : files){
+			try {
+				loadDataFromFile(s);
+			} catch (FileNotFoundException e) {
+				System.out.println("Cannot open file: " + s);//Prints a more user friendly message then e.printStackTrace
+			}
+			printInOrder();
+			System.out.println("\n");
 		}
-		printInOrder();
 	}
 	
 	public static void main(String [] args) {
